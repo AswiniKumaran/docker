d78178253d0b4a62fb3920da6a541fedd932ccab
AswiniKumaran <Aswini.Kumaran@lumenaza.de>
2026-02-04 21:02:17 +0100

    add env vars

--- learn_docker/settings.py
+++ learn_docker/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['my-django-app-mew0.onrender.com']
 
